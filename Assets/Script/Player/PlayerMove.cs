549990a75d77456caa4fcd02e5c6e1daa84e9823
nelu572 <s25031@gsm.hs.kr>
2025-10-01 16:13:52 +0900

    캐릭터 움직임 + 카메라 이동 테스트

--- Assets/Script/Player/PlayerMove.cs
+++ Assets/Script/Player/PlayerMove.cs
@@ -1,58 +1,56 @@
+
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    [SerializeField] private GameObject GM;
+    Inputs input;
+
     Rigidbody2D rb;
 
-    public float jumpForce = 10;
-    public float maxSpeed = 10;
-    public float acceleration = 1;
+    [SerializeField] private float jumpForce = 10;
+    [SerializeField] private float maxSpeed = 10;
+    [SerializeField] private float acceleration = 1;
     float speed = 0;
     float prev_speed = 0;
+
     bool onGround;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        input = GM.GetComponent<Inputs>();
     }
 
     void Update()
     {
 
-        RaycastHit2D hit = Physics2D.Raycast(rb.position, Vector3.down, transform.localScale.y / 2);
-        Debug.DrawRay(rb.position, Vector2.down * 1, Color.red);
-        if (hit.collider != null)
-        {
-            Debug.Log(hit.collider.name);
-        }
     }
     void FixedUpdate()
     {
         Move();
-        if (Input.GetKey(KeyCode.Space))
-        {
-            Jump();
-        }
+        Jump();
     }
+
     void Move()
     {
-        int dir = 0;
-        if (Input.GetKey(KeyCode.A)) dir -= 1;
-        if (Input.GetKey(KeyCode.D)) dir += 1;
-
         prev_speed = rb.linearVelocity.x;
-        if (dir == 0)
+        bool left = input.Get_Keys(KeyCode.A);
+        bool right = input.Get_Keys(KeyCode.D);
+        if (!(left ^ right))
         {
             if (speed != 0)
             {
-                speed = Mathf.MoveTowards(speed, 0, acceleration);
+                speed = Mathf.MoveTowards(speed, 0, acceleration * 2);
             }
         }
         else
         {
+            int dir = 0;
+            if (left) dir = -1;
+            else dir = 1;
             speed = prev_speed + dir * acceleration;
             speed = Mathf.Clamp(speed, -maxSpeed, maxSpeed);
         }
-        Debug.Log(speed);
         rb.linearVelocity = new Vector2(speed, rb.linearVelocity.y);
     }
     void Jump()
@@ -62,6 +60,8 @@ public class PlayerMove : MonoBehaviour
     }
     void OnGround_check()
     {
-
+        RaycastHit2D[] hit = Physics2D.RaycastAll(rb.position, Vector3.down, transform.localScale.y);
+        Debug.DrawRay(rb.position, Vector2.down * 1, Color.red);
+        
     }
 }
